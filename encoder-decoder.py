02ccab8eec5cf864aecafc1a18ad7b38aa572243
danielacamargo <daniela.camargos@ufl.edu>
2023-03-08 19:58:33 -0600

    decode function

--- encoder-decoder.py
+++ encoder-decoder.py
@@ -7,7 +7,14 @@ def encode(data):
     return ''.join(encoded)
     
 def decode(data):
-    pass
+
+    decoded_password = ""
+    for digit in data:
+        # Cast digit to an int and subtract 3. % 10 to get the first digit
+        digit = (int(digit) - 3) % 10
+        # Add each digit to a new string
+        decoded_password += str(digit)
+    return decoded_password
 
 def show_menu():
     print('1. Encode')
@@ -16,12 +23,14 @@ def show_menu():
     return input('Please enter your selection: ')
 
 def main():
-    while True:
-        choice = int(show_menu())
+
+    choice = 0
+    while choice != 3:      # the loop has to end as soon as the user inputs 3
+        choice = int(show_menu())       # we don't need the int as we already defined choice as an integer = '0'
         if choice == 1:
             password = input('Please enter a password to encode: ')
             encoded = encode(password)
-            print(f'Your encoded password is: {encoded}')
+            print(f'Your encoded password is: {encoded}')       # i don't we have to show the encoded password
         elif choice == 2:
             password = input('Please enter a password to decode: ')
             decoded = decode(password)
